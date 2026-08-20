831d7cdff64c653964c79ccc0abb4b8494c764f0
praveensanthosham17-cyber <praveensanthosham17@gmail.com>
2026-08-20 15:10:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,22 @@
-// Your first Java program
-public class HelloWorld {
+import java.util.Scanner;
+
+public class Addition {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Calculate sum
+        int sum = num1 + num2;
+
+        // Display result
+        System.out.println("Sum = " + sum);
+
+        sc.close();
     }
 }
